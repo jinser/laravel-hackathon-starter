03449b20bea80bcd2506c41133a021e06cf720dc
Jin Ser <djin33@gmail.com>
2016-07-13 03:00:37 +0000

    added credit card validity check in subscription api

--- app/Http/Controllers/Api/SubscriptionApiController.php
+++ app/Http/Controllers/Api/SubscriptionApiController.php
@@ -31,10 +31,8 @@ class SubscriptionApiController extends Controller
         $test = $user->stripe_id;
         $this->_setStripeKey();
         if($test == null) {
-            $response = [
-                'error' => 'user has no stripe id'
-            ];
-            return response()->json($response);
+            $response = $this->_getErrorResponseJSON('user has no stripe id.');
+             return response()->json($response);
         }
         
         $response = \Stripe\Customer::retrieve($test);
@@ -67,9 +65,45 @@ class SubscriptionApiController extends Controller
     public function store(Request $request) {
         $user = \Auth::user();
        
+        //check that user has a credit card already stored
+        
+        
         //if user is subscribed, update the plan
         if($user->subscribed()) {
             $success = 'success';
+        }
+        $userStripeId = $user->stripe_id;
+        $this->_setStripeKey();
+        $stripeCustomer = \Stripe\Customer::retrieve($userStripeId);
+        $custCards = $stripeCustomer->sources->data;
+        
+        //check if customer has credit card info
+        if(count($custCards) == 0) {
+             $response = $this->_getErrorResponseJSON('no credit card information found. Please update your credit card information.');
+             return response()->json($response);
+        }
+        
+        //check validity of credit cards
+        $numOfFailedCards = 0;
+        $numOfStoredCards = 0;
+        
+        for($i=0;$i<count($custCards);$i++) {
+            $custCardsect = $custCards[$i];
+            
+            //object is not a credit card
+            if(strcmp($custCards[$i]->object,'card') != 0) {
+                $numOfStoredCards++;
+            }
+            
+            //credit card validity check failed
+            if(strcmp($custCards[$i]->cvc_check,'pass') != 0) {
+                $numOfFailedCards++;
+            }
+        }
+        if($numOfFailedCards == count($custCards) || $numOfStoredCards == count($custCards)) {
+            //return all credit cards failed, need to add a new credit card
+            $response = $this->_getErrorResponseJSON('invalid credit card information found. Please update your credit card information.');
+             return response()->json($response);
         }
         
         //get plan details
@@ -82,16 +116,12 @@ class SubscriptionApiController extends Controller
         //check that plan is live
         $response = \Stripe\Plan::retrieve($request->planid);
         if($response == null) {
-            $finalResponse = [
-                    'error' => 'plan is not live.'
-                ];
-            return response()->json($finalResponse);   
+            $response = $this->_getErrorResponseJSON('plan is not live.');
+             return response()->json($response);
         }
        
         //create sample token if does not exist
         if($user->card_brand == null) {
-            $this->_setStripeKey();
-            
             $creditCardToken = \Stripe\Token::create(array(
               "card" => array(
                 "number" => "4242424242424242",
@@ -100,15 +130,22 @@ class SubscriptionApiController extends Controller
                 "cvc" => "314"
               )
             ));
+            
+            $user->newSubscription(
+                $plan->name,
+                $request->planid
+            )->create($creditCardToken->id);
         }
         
         //create subscription with laravel cashier
+        
         $user->newSubscription(
             $plan->name,
             $request->planid
-        )->create($creditCardToken->id);
+        )->create();
         //first param is plan name e.g. stripe test 2
         //second param is the id of the plan in Stripe (id field, also is id of pricing_plans table )
+        //creditCardToken can be null, if not null it will update the Stripe customer credit card details
         
         return response()->json($user);
     }
@@ -133,7 +170,14 @@ class SubscriptionApiController extends Controller
         return response()->json($charge);
     }
     
-     private function _setStripeKey() {
+    private function _setStripeKey() {
         \Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
+    }
+    
+    private function _getErrorResponseJSON($errorMsg) {
+         $finalResponse = [
+                    'error' => $errorMsg
+                ];
+            return $finalResponse;
     }
 }
